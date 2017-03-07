d492408fe1e2a60b41bedc650d6c08884d26f0ce
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2017-03-07 08:08:57 +0800

    Create script that will create a PostgreSQL dump

---
+++ PostgreSQL/create_sql_dump.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -eq 0]
+then
+  echo "No arguments passed. USAGE: ./create_sql_dump <USER> <HOST> <OUTPUT_FILE>"
+  exit 2
+else
+  $USER=$1
+  $HOST=$2
+  $DATABASE=$3
+  $OUTPUT_FILE=$4
+
+  pg_dump -U $USER -h $HOST $DATABASE > $OUTPUT_FILE
+ fi
