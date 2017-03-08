7d6e404c55cb6a1de1b90369daf2eff78f8b9342
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2017-03-08 09:44:29 +0800

    Add additional comments
    Short description for the purpose of the script and basic usage instructions.

--- PostgreSQL/pg_kill_open_connections.sql
+++ PostgreSQL/pg_kill_open_connections.sql
@@ -1,1 +1,4 @@
+-- This SQL command will terminate all open connections to the selected database.
+-- Replace database_name with the name of your database
+
 SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname = 'database_name' AND pid <> pg_backend_pid();
